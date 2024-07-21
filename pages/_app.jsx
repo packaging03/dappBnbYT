ed3713f09f6223489771c983f79542760f1a699d
Adeyemi Olaoye <yhemi06@gmail.com>
2024-07-21 22:12:55 +0100

    finished configuring redux

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -6,6 +6,8 @@ import 'react-datepicker/dist/react-datepicker.css'
 import { useEffect, useState } from 'react'
 import Providers from '@/services/provider'
 import { Footer, Header } from '@/components'
+import { Provider } from 'react-redux'
+import { store } from '../store/index'
 
 export default function App({ Component, pageProps }) {
   const [showChild, setShowChild] = useState(false)
@@ -19,6 +21,7 @@ export default function App({ Component, pageProps }) {
   } else {
     return (
       <Providers pageProps={pageProps}>
+        <Provider store={store}></Provider>
         <div className="relative h-screen min-w-screen">
           <Header />
           <Component {...pageProps} />
